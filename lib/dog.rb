9d234e8fe42dd50a7b4d63fe44f70154e65767e3
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-29 22:19:50 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+
+class Dog
+
+ 
+  def name=(its_name)
+    @name = its_name
+  end
+ 
+  def name
+    @name
+  end
+ 
+   def breed=(its_breed)
+    @breed = its_breed
+  end
+ 
+  def breed
+    @breed
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+
+class Person
+
+ 
+  def name=(his_name)
+    @name = his_name
+  end
+ 
+  def name
+    @name
+  end
+ 
+   def job=(his_job)
+    @job = his_job
+  end
+ 
+  def job
+    @job
+  end
+  
+end
