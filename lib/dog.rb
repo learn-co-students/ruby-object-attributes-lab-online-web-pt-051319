62670041e34cea83083a3801d837ad40da08efba
James Britton <brittonjp@gmail.com>
2019-06-04 18:54:29 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,35 +1,19 @@
 class Dog
-  def name=(dog_name)
-    @name = dog_name
+  
+  def name=(name)
+    @name = name
   end
   
   def name
     @name
   end
   
-  def breed=(dog_breed)
-    @breed = dog_breed
+  def breed=(breed)
+    @breed = breed
   end
   
   def breed
     @breed
   end
-end
-
-class Person
-  def name=(person_name)
-    @name = person_name
-  end
   
-  def name
-    @name
-  end
-  
-  def job=(persons_job)
-    @job = persons_job
-  end
-  
-  def job
-    @job
-  end
 end
